027af31a385a86fca4c81c7e200a877d8f3702e1
rgheda <r.gheda@student.tudelft.nl>
2023-10-03 21:27:57 +0200

    Poisson image editing and Anisotropic implemented in C++. Automatic scribble expansion implemented

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ import tkinter as tk
 from tkinter import filedialog as fd
 from PIL import ImageTk, Image
 from poisson import solve_poisson, anisotropic_solver
+import os
+import subprocess
 
 SEL_IMAGE = None
 SUPPORTED_FORMATS = ["JPG", "JPEG", "PNG", "jpg", "jpeg", "png"]
@@ -12,6 +14,7 @@ depth_annotation_windowd = None
 canvas = None
 out = None
 scribbles = {}
+THICKNESS = 2
 
 def select_image():
     global SEL_IMAGE, img
@@ -44,11 +47,13 @@ def draw_handler(event):
     canvas.create_line((lasx, lasy, event.x, event.y), 
                         fill=color, 
                         width=3)
-    scribbles[(lasx, lasy)] = depth_slider.get()
+    for i in range(int(-THICKNESS / 2), int(THICKNESS /2)):
+        for j in range(int(-THICKNESS / 2), int(THICKNESS /2)):
+            scribbles[(lasy+j, lasx+i)] = depth_slider.get()
     lasx, lasy = event.x, event.y
 
 def run():
-    global depth_annotation_windowd, canvas, depth_slider
+    global depth_annotation_windowd, canvas, depth_slider, scribbles
     depth_annotation_windowd = tk.Tk()
     depth_annotation_windowd.title("Draw")
     
@@ -64,22 +69,69 @@ def run():
     depth_slider.set(0)
     depth_slider.pack()
 
+    scribbles = {}
+
     depth_annotation_windowd.mainloop()
+
+def save_scribbles():
+    if os.path.exists("../outputs/scribbles"):
+        os.remove("../outputs/scribbles")
+    
+    with open("../outputs/scribbles", "w") as f:
+        for key, value in scribbles.items():
+            f.write(str(key[0]) + " " + str(key[1]) + " " + str(value) + "\n")
 
 def run_poisson():
     global img, scribbles
-    img = img.convert('L')
-    solve_poisson(img, scribbles)
+    if not os.path.exists("../outputs"):
+        os.makedirs("../outputs")
+    img.save("../outputs/src_rgb.png")
+    grey_img = img.convert('L')
+    grey_img.save("../outputs/greyscale-input.png")
+
+    save_scribbles()
+    print(iterations)
+
+    arglist = ["../build/a1_hdr", "../outputs/greyscale-input.png", "../outputs/src_rgb.png", "../outputs/poisson_out.png", "../outputs/scribbles", "poisson", iterations, beta]
+    proc = subprocess.Popen(arglist, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = proc.communicate()
+    print(stdout)
+    print(stderr)
+    proc.wait()
+    print('Done')
+    out = Image.open("../outputs/poisson_out.png")
+    out.show()
+    # solve_poisson(img, scribbles)
 
 def run_anisotropic():
     global img, scribbles
-    img = img.convert('L')
-    anisotropic_solver(img, scribbles)
+    if not os.path.exists("../outputs"):
+        os.makedirs("../outputs")
+    img.save("../outputs/src_rgb.png")
+    grey_img = img.convert('L')
+    grey_img.save("../outputs/greyscale-input.png")
+
+    save_scribbles()
+
+    arglist = ["../build/a1_hdr", "../outputs/greyscale-input.png", "../outputs/src_rgb.png", "../outputs/anisotropic_out.png", "../outputs/scribbles", "anisotropic", iterations, beta]
+    proc = subprocess.Popen(arglist, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = proc.communicate()
+    print(stdout)
+    print(stderr)
+    proc.wait()
+    print('Done')
+    out = Image.open("../outputs/anisotropic_out.png")
+    out.show()
+
+def update_iter(event):
+    global iterations
+    iterations = event.widget.get()
+
+def update_beta(event):
+    global beta
+    beta = event.widget.get()
 
 window = tk.Tk()
-
-greeting = tk.Label(text="Hello, Tkinter")
-greeting.pack()
 
 #Create a button that lets to select an image file
 image_button = tk.Button(
@@ -89,8 +141,7 @@ image_button = tk.Button(
     bg="blue",
     fg="yellow",
     command= select_image
-)
-image_button.pack()
+).grid(row=0, column=0, columnspan=2)
 
 draw_button = tk.Button(
     text="Draw",
@@ -99,8 +150,7 @@ draw_button = tk.Button(
     bg="green",
     fg="yellow",
     command= run
-)
-draw_button.pack()
+).grid(row=1, column=0, columnspan=2)
 
 poisson_button = tk.Button(
     text="Poisson",
@@ -109,8 +159,7 @@ poisson_button = tk.Button(
     bg="red",
     fg="yellow",
     command= run_poisson
-)
-poisson_button.pack()
+).grid(row=2, column=0, columnspan=2)
 
 anisotropic_button = tk.Button(
     text="Anisotropic",
@@ -119,7 +168,16 @@ anisotropic_button = tk.Button(
     bg="red",
     fg="yellow",
     command= run_anisotropic
-)
-anisotropic_button.pack()
+).grid(row=3, column=0, columnspan=2)
+
+iter_label = tk.Label(text="Number of iterations").grid(row=4, column=0)
+n_of_iter = tk.Entry()
+n_of_iter.bind("<Return>", update_iter)
+n_of_iter.grid(row=4, column=1)
+
+beta_label = tk.Label(text="Beta").grid(row=5, column=0)
+beta_entry = tk.Entry()
+beta_entry.bind("<Return>", update_beta)
+beta_entry.grid(row=5, column=1)
 
 window.mainloop()
---
+++ src/your_code_here.h
@@ -0,0 +1,267 @@
+#pragma once
+#include <algorithm>
+#include <array>
+#include <cmath>
+#include <numeric>
+#include <span>
+#include <tuple>
+#include <map>
+#include <vector>
+
+#include "helpers.h"
+#include <iostream>
+#include <math.h>
+#include <cstdlib>
+
+const float BETA = 20.0;
+const int NUM_ITERS = 4000;
+const float TOLERANCE = 1 / 255.0f;
+const float RGB_TO_GREY[3] = {0.2989, 0.5870, 0.1140};
+
+template<typename T>
+int getImageOffset(const Image<T>& image, int x, int y)
+{
+    return x + (y * image.width);
+}
+
+float pixel_coeff(int x_1, int y_1, int x_2, int y_2, const ImageFloat& img, float beta){
+    if (! (x_1 >= 0 && x_1 < img.width && y_1 >= 0 && y_1 < img.height &&
+        x_2 >= 0 && x_2 < img.width && y_2 >= 0 && y_2 < img.height)) {
+        return 0;
+    }
+
+    float diff = 0;
+    auto pxl_1 = img.data[getImageOffset(img, x_1, y_1)];
+    auto pxl_2 = img.data[getImageOffset(img, x_2, y_2)];
+
+    diff = abs(pxl_1 - pxl_2);
+    
+    // diff = abs((pxl_1.r + pxl_1.g + pxl_1.b) - (pxl_2.r + pxl_2.g + pxl_2.b));
+    return exp(-beta * diff); 
+}
+
+ImageFloat solvePoisson(const ImageFloat& input_image, std::map<int, float>  scribbles, std::vector<std::vector<bool>> lookup, const int num_iters = NUM_ITERS)
+{
+    // Empty solution.
+    auto I = ImageFloat(input_image.width, input_image.height);
+    // std::fill(I.data.begin(), I.data.end(), 0.0f);
+    
+    // Another solution for the alteranting updates.
+    auto I_next = ImageFloat(input_image.width, input_image.height);
+    
+    for (int i = 0;i < I.width * I.height;++i)
+    {
+        I.data[i] = input_image.data[i];
+        I_next.data[i] = input_image.data[i];
+        
+        if (lookup[i % I.width][i / I.width]) {
+            I.data[i] = scribbles[i] ; // / 255.0f;
+            I_next.data[i] = scribbles[i] ; // / 255.0f;
+        }
+    }
+
+    // Iterative solver.
+    for (auto iter = 0; iter < num_iters; iter++)
+    {
+
+        int X = input_image.width;
+        int Y = input_image.height;
+        
+        std::vector<std::vector<int>> new_lookups;
+        #pragma parallel for shared(I, I_next)
+        for (int x = 0;x < X; ++x)
+        {
+            #pragma parallel for shared(I, I_next)
+            for (int y = 0;y < Y; ++y)
+            {
+                if (lookup[x][y]) {
+                    // I_next.data[getImageOffset(I_next, x, y)] = scribbles[getImageOffset(input_image, x, y)] ; // / 255.0f;
+                    continue;
+                }
+
+                float prv_x = 0;
+                float nxt_x = 0;
+                float prv_y = 0;
+                float nxt_y = 0;
+
+                if (x > 0)
+                {
+                    prv_x = I.data[getImageOffset(I, x - 1, y)];
+                    if ((lookup[x-1][y]) and ((abs(prv_x - I.data[getImageOffset(I, x, y)])) < TOLERANCE)) {
+                        I.data[getImageOffset(I, x, y)] = prv_x;
+                        I_next.data[getImageOffset(I_next, x, y)] = prv_x;
+                        std::vector<int> tmp = {x, y};
+                        new_lookups.push_back(tmp);
+                        continue;
+                    }
+                }
+                if (x < (X - 1))
+                {
+                    nxt_x = I.data[getImageOffset(I, x + 1, y)];
+                    if ((lookup[x+1][y]) and ((abs(nxt_x - I.data[getImageOffset(I, x, y)])) < TOLERANCE)) {
+                        I.data[getImageOffset(I, x, y)] = nxt_x;
+                        I_next.data[getImageOffset(I_next, x, y)] = nxt_x;
+                        std::vector<int> tmp = {x, y};
+                        new_lookups.push_back(tmp);
+                        continue;
+                    }
+                }
+                if (y > 0)
+                {
+                    prv_y = I.data[getImageOffset(I, x, y - 1)];
+                    if ((lookup[x][y-1]) and ((abs(prv_y - I.data[getImageOffset(I, x, y)])) < TOLERANCE)) {
+                        I.data[getImageOffset(I, x, y)] = prv_y;
+                        I_next.data[getImageOffset(I_next, x, y)] = prv_y;
+                        std::vector<int> tmp = {x, y};
+                        new_lookups.push_back(tmp);
+                        continue;
+                    }
+                }
+                if (y < (Y - 1))
+                {
+                    nxt_y = I.data[getImageOffset(I, x, y + 1)];
+                    if ((lookup[x][y+1]) and ((abs(nxt_y - I.data[getImageOffset(I, x, y)])) < TOLERANCE)) {
+                        I.data[getImageOffset(I, x, y)] =   nxt_y;
+                        I_next.data[getImageOffset(I_next, x, y)] = nxt_y;
+                        std::vector<int> tmp = {x, y};
+                        new_lookups.push_back(tmp);
+                        continue;
+                    }
+                }
+
+
+                I_next.data[getImageOffset(I_next, x, y)] = ( prv_x + nxt_x + prv_y + nxt_y) / 4.0;
+            }
+        }
+
+        for (auto i = 0; i < new_lookups.size(); i++) {
+            lookup[new_lookups[i][0]][new_lookups[i][1]] = true;
+        }
+
+        // Swaps the current and next solution so that the next iteration
+        // uses the new solution as input and the previous solution as output.
+        std::swap(I, I_next);
+    }
+
+    // After the last "swap", I is the latest solution.
+    return I;
+}
+
+
+ImageFloat solveAnisotropic(const ImageFloat& input_image, const ImageRGB& input_rgb, std::map<int, float>  scribbles, std::vector<std::vector<bool>> lookup, const int num_iters = NUM_ITERS, const float beta = BETA)
+{
+    // Empty solution.
+    auto I = ImageFloat(input_image.width, input_image.height);
+    
+    // Another solution for the alteranting updates.
+    auto I_next = ImageFloat(input_image.width, input_image.height);
+    
+    for (int i = 0;i < I.width * I.height;++i)
+    {
+        I.data[i] = input_image.data[i];
+        I_next.data[i] = input_image.data[i];
+        
+        if (lookup[i % I.width][i / I.width]) {
+            I.data[i] = scribbles[i] ; // / 255.0f;
+            I_next.data[i] = scribbles[i] ; // / 255.0f;
+            // std::cout << i << " " << I.data[i] << std::endl;
+            // std::cout << i << " " << scribbles[i] << std::endl;
+        }
+    }
+
+    
+    std::cout << "Solving Anisotropic equation..." << std::endl;
+    // Iterative solver.
+    for (auto iter = 0; iter < num_iters; iter++)
+    {
+        int X = input_image.width;
+        int Y = input_image.height;
+
+        std::vector<std::vector<int>> new_lookups;
+        
+        #pragma parallel for shared(I, I_next)
+        for (int x = 0;x < X; ++x)
+        {
+            #pragma parallel for shared(I, I_next)
+            for (int y = 0;y < Y; ++y)
+            {
+                if (lookup[x][y]) {
+                    // I_next.data[getImageOffset(I_next, x, y)] = scribbles[getImageOffset(input_image, x, y)];
+                    continue;
+                }
+                float prv_x = 0;
+                float nxt_x = 0;
+                float prv_y = 0;
+                float nxt_y = 0;
+
+                float w_prv_x = 0;
+                float w_nxt_x = 0;
+                float w_prv_y = 0;
+                float w_nxt_y = 0;
+
+                if (x > 0)
+                {
+                    prv_x = I.data[getImageOffset(I, x - 1, y)];
+                    w_prv_x = pixel_coeff(x, y, x - 1, y, input_image, beta);
+                    if ((lookup[x-1][y]) and (abs(prv_x - I.data[getImageOffset(I, x, y)]) < TOLERANCE)) {
+                        I.data[getImageOffset(I, x, y)] = prv_x;
+                        I_next.data[getImageOffset(I, x, y)] = prv_x;
+                        std::vector<int> tmp = {x, y};
+                        new_lookups.push_back(tmp);
+                        continue;
+                    }
+                }
+                if (x < (X - 1))
+                {
+                    nxt_x = I.data[getImageOffset(I, x + 1, y)];
+                    w_nxt_x = pixel_coeff(x, y, x + 1, y, input_image, beta);
+                    if ((lookup[x+1][y]) and (abs(nxt_x - I.data[getImageOffset(I, x, y)]) < TOLERANCE)) {
+                        I.data[getImageOffset(I, x, y)] = nxt_x;
+                        I_next.data[getImageOffset(I, x, y)] = nxt_x;
+                        std::vector<int> tmp = {x, y};
+                        new_lookups.push_back(tmp);
+                        continue;
+                    }
+                }
+                if (y > 0)
+                {
+                    prv_y = I.data[getImageOffset(I, x, y - 1)];
+                    w_prv_y = pixel_coeff(x, y, x, y - 1, input_image, beta);
+                    if ((lookup[x][y-1]) and (abs(prv_y - I.data[getImageOffset(I, x, y)]) < TOLERANCE)) {
+                        I.data[getImageOffset(I, x, y)] = prv_y;
+                        I_next.data[getImageOffset(I, x, y)] = prv_y;
+                        std::vector<int> tmp = {x, y};
+                        new_lookups.push_back(tmp);
+                        continue;
+                    }
+                }
+                if (y < (Y - 1))
+                {
+                    nxt_y = I.data[getImageOffset(I, x, y + 1)];
+                    w_nxt_y = pixel_coeff(x, y, x, y + 1, input_image, beta);
+                    if ((lookup[x][y+1]) and ((abs(nxt_y - I.data[getImageOffset(I, x, y)])) < TOLERANCE)) {
+                        I.data[getImageOffset(I, x, y)] =   nxt_y;
+                        I_next.data[getImageOffset(I_next, x, y)] = nxt_y;
+                        std::vector<int> tmp = {x, y};
+                        new_lookups.push_back(tmp);
+                        continue;
+                    }
+                }
+
+                I_next.data[getImageOffset(I_next, x, y)] = ((w_prv_x * prv_x + w_nxt_x * nxt_x + w_prv_y * prv_y + w_nxt_y * nxt_y)) / (w_prv_x + w_nxt_x + w_prv_y + w_nxt_y);
+                // I_next.data[getImageOffset(I_next, x, y)] = (( prv_x + nxt_x + prv_y + nxt_y)) / 4.0;
+            }
+        }
+
+        for (auto i = 0; i < new_lookups.size(); i++) {
+            lookup[new_lookups[i][0]][new_lookups[i][1]] = true;
+        }
+
+        // Swaps the current and next solution so that the next iteration
+        // uses the new solution as input and the previous solution as output.
+        std::swap(I, I_next);
+    }
+
+    // After the last "swap", I is the latest solution.
+    return I;
+}
