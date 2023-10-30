ce6287c056ac0dced05f07393db1a57d412b899d
rgheda <robertogheda.rg@gmail.com>
2023-10-30 15:39:10 +0100

    code cleaning

--- src/parallax_main.cpp
+++ src/parallax_main.cpp
@@ -4,26 +4,6 @@
 #include <filesystem>
 #include <iostream>
 
-ImageRGB disparityToColor(const ImageFloat& disparity, const float min_val = 0.0f, const float max_val = 1.0f) {
-    const glm::vec3 color_neg = glm::vec3(26, 56, 89) / 255.0f;
-    const glm::vec3 color_zero = glm::vec3(255, 255, 255) / 255.0f;
-    const glm::vec3 color_pos = glm::vec3(247, 190, 29) / 255.0f;
-
-    auto res = ImageRGB(disparity.width, disparity.height);
-    
-    #pragma omp parallel for
-    for (auto i = 0; i < disparity.data.size(); i++) {
-        auto val = (disparity.data[i] - min_val) / (max_val - min_val);
-        if (val < 0.0) {
-            res.data[i] = glm::mix(color_zero, color_neg, -val);
-        } else {
-            res.data[i] = glm::mix(color_zero, color_pos, val);        
-        }
-    }
-
-    return res;
-}
-
 int main(int argc, char** argv)
 {
     printOpenMPStatus();
@@ -66,7 +46,7 @@ int main(int argc, char** argv)
 
     for (int i = - frames / 2; i < frames / 2; ++i)
     {
-        // Forward warp the image. We use the scaled disparity.
+        // Forward warp the image
         auto img_forward = forwardWarpImage(image, depth_map, target_disparity, i*warp_factor);
         // Inpaint the holes in the forward warping image.
         ImageRGB dst_image = inpaintHoles(img_forward, 19);
